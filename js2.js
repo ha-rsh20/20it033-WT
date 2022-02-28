47fb4f8bf9362698a3336b28a3c509d8486b63b9
ha-rsh20 <88107325+ha-rsh20@users.noreply.github.com>
2022-02-28 23:02:07 +0530

    Delete js2.js

--- js2.js
+++
@@ -1,8 +0,0 @@
-function print(){
-    x = parseInt(prompt("Enter the value:"));
-    let n = "";
-    for(i=1;i<=x;i++){
-        n = n+i+"<br>"
-    }
-    document.getElementById("txt").innerHTML = n;
-}
