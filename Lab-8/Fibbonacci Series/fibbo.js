e2ab2bdb8901fc2a80a165d3b874ad1a885bc7c4
ha-rsh20 <88107325+ha-rsh20@users.noreply.github.com>
2022-03-13 22:37:06 +0530

    Update fibbo.js

--- Lab-8/Fibbonacci Series/fibbo.js
+++ Lab-8/Fibbonacci Series/fibbo.js
@@ -5,18 +5,30 @@ function cal(){
     n = "";
     if(x!=0){
         if(x>=1){
-            n = n+"f(1):0<br>";
+            n = n+"f(1):  0<br>";
         }
         if(x>1){
-            n = n+"f(2):1<br>";
+            n = n+"f(2):  <span>1</span><br>";
         }
     }
     x=x-2;
     for(i=0;i<x;i++){
+        prime = 0;
         c = a+b;
         a = b;
         b = c;
-        n = n+"f("+(i+3)+"):"+c+"<br>";
+        for(j=1;j<=c;j++)
+        {
+            if(c%j==0){
+                prime++;
+            }
+        }
+        if(prime<=2){
+            n = n+"f("+(i+3)+"):  <span>"+c+"</span><br>";
+        }
+        else{
+            n = n+"f("+(i+3)+"):  "+c+"<br>";
+        }
     }
-    document.getElementById("fibbo").innerHTML = n;
+    document.getElementById("fibbo").innerHTML = n; 
 }
